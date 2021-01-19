c876d5b8e5d8b62bb8581788fb16e5d00fbc370c
CarlosJesus <c.sipiran07@gmail.com>
2021-01-18 21:40:29 -0300

    se elimina la class project y se agrega el objeto json de proyectos, tambien se agrega pag de Proyectos y se hace DOM y eventos

--- funciones.js
+++ funciones.js
@@ -1,27 +1,40 @@
-class Project
-{
-   id = 0;
-   titulo = '';
-   descripcion = '';
-   tecnologias = ['css','html','js','python','c++','c','java','bootstrap','.net framework '];
+window.addEventListener('load',() =>{
 
-    Project(id,titulo,descripcion)
-    {
-        this.id = id;
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-    }
+    /* Se hace esta funcion para optimizar la creación de tag <div> en la plantilla de Proyectos.html */
+    const addProject = document.getElementById('box-project'); 
+    proyectos.forEach(element => {
+        addProject.innerHTML += ` <h2> <b>${element.nombre}</b> </h2>
+                                    <h6> <b>${element.descripcion}</b> </h6>
+                                    <h6> <b> Tecnologías :  ${element.tecnologias} </b></h6>
+                                    <a href="${element.codigofuente}"><h6>Repositorio del Codigo Fuente Aqui</h6></a> 
+                                    <video controls autoplay> <source src="${element.video}" 
+                                    type="video/mp4" </video> `
+    });
 
-    addProject()
-    {
-        //se agrrgará los proyectos correspondientes indicando las
-        //tecnologias usadas.
-    }
-}
+    const nav = document.querySelectorAll('.categoria nav ul li a');
+    nav.forEach((e) =>{
+       
+        e.addEventListener('click',(event)=>{
+            event.preventDefault();
+            e.classList.remove('active');
+            event.target.classList.add('active');
+            
+            const categorias = event.target.innerHTML.toLowerCase()
+            /* e.filter(`[class = "${categorias}"]`); */
+            console.log(categorias);
 
+        });
+    })
+
+
+
+/*DOM que se creo para hacer el popup 
 let modal = document.getElementById('idModal');
 let close = document.getElementById('btnClose');
 
 close.addEventListener('click',function(){
     modal.style.display = 'none';
+}) 
+se comenta por que ya no se precisa*/
+
 })
